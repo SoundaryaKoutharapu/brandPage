35c29ac721dbb2b7f0a0940c3f9d729ecca785a1
Soundarya Koutharapu <soundaryakoutharapu@gmail.com>
2023-05-17 13:32:19 +0530

    hero section

--- brand_page/src/App.jsx
+++ brand_page/src/App.jsx
@@ -2,12 +2,14 @@
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import './App.css'
+import HeroSection from './components/HeroSection';
 import Navigation from './components/Navigation';
 
 const App = () => {
   return (
     <div>
     <Navigation/>
+    <HeroSection/>
    </div>
   ); 
 };
---
+++ brand_page/src/components/HeroSection.jsx
@@ -0,0 +1,38 @@
+const HeroSection = () => {
+    return (
+        <main className="hero">
+
+            <div className="hero-content" >
+
+                <h1>
+                    YOUR FEET DESERVE THE BEST
+                </h1>
+
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ab nisi debitis voluptatum eum similique.
+                    Assumenda soluta dignissimos porro, et est sint rem hic aperiam
+                    quisquam saepe. Exercitationem, inventore
+                </p>
+
+                <div className="hero-btn">
+                    <button>Shop Now</button>
+                    <button>Category</button>
+                </div>
+
+                <div className="shop">
+                    <p> also available on</p>
+
+                    <div className="icons">
+                        <img src="./images/amazon.png" alt="amazon" />
+                        <img src="./images/flipkart.png" alt="flipcart" />
+                    </div>
+                </div>
+
+            </div>
+
+            <div className="hero-img">
+                <img src="./images/shoe_image.png" alt="shoe" />
+            </div>
+        </main>
+    )
+}
+export default HeroSection
